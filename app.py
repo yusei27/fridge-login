3bed152d42a8e272ce6125a9fa0df21c2a32cb4a
yusei-hashimoto <syutard@yahoo.co.jp>
2024-02-28 22:20:52 +0900

    CORS対策

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+#特定のオリジンだけを許可する
+cors = CORS(app, resources={r"/*":{"origin": ["http://localhost:5173"]}})
 
 @app.route('/register/user')
 def register_user():
