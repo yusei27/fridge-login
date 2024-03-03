ab067e5bd109a2ee33259dcf2b31d3ce64e422bc
yusei-hashimoto <syutard@yahoo.co.jp>
2024-03-03 22:40:58 +0900

    db接続用汎用クラス追加

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask import request
+import db.db_connect_postgresql as db
 app = Flask(__name__)
 #特定のオリジンだけを許可する
 cors = CORS(app, resources={r"/*":{"origin": ["http://localhost:5173"]}})
 
-@app.route('/register/user')
+@app.route('/register/user', methods=["POST"])
 def register_user():
     print("ユーザー登録API実行")
+    requeat_data = request.get_json()
+    print("リクエスト", requeat_data)
+    hash_password("yusei9981")
+
+    print("db接続")
+    db_connect = db.DbConnectPostgres()
+    
     return jsonify({'status':200, 'data':3})
 
+
+def hash_password(password):
+    import os
+    import base64
+    import hashlib
+    
+    salt = os.urandom(32)
+    print("salt", salt)
+    salt_encode = base64.b64encode(salt)
+
+    hash_value = hashlib.sha256(password.encode() + salt_encode)
+    print(hash_value)
+    print("hash16", hash_value.hexdigest())#ハッシュオブジェクトからハッシュ値(16進数文字列)取得,sha256は16進数もじれt５うを生成する仕組み
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3333, debug=True)
---
+++ db/db_connect_postgresql.py
@@ -0,0 +1,90 @@
+import psycopg2
+import psycopg2.extras
+import configparser
+import os
+import errno
+
+class DbConnectPostgres:
+    """
+    Postgresqlにアクセスするための汎用クラス
+    以下のサイトを参考に作成
+    https://tech.nkhn37.net/python-psycopg2-postgresql-dbaccess/
+    """
+    def __init__(self) -> None:
+        """コンストラクタ
+        :return: None
+        """
+        # コンフィグファイルからデータを取得
+        config_db = configparser.ConfigParser()
+        config_ini_path = "./config/dbconfig.ini"
+
+        # 指定したiniファイルが存在しない場合、エラー発生
+        if not os.path.exists(config_ini_path):
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config_ini_path)
+        config_db.read(config_ini_path)
+        print(config_db)
+        # 接続情報の取得
+        host = config_db["POSTGRESSQL_DB_SERVER"]["host"]
+        port = config_db["POSTGRESSQL_DB_SERVER"]["port"]
+        dbname = config_db["POSTGRESSQL_DB_SERVER"]["dbname"]
+        user = config_db["POSTGRESSQL_DB_SERVER"]["user"]
+        password = config_db["POSTGRESSQL_DB_SERVER"]["password"]
+        # コネクションを確立する
+        self.con = psycopg2.connect(
+            host=host, port=port, dbname=dbname, user=user, password=password
+        )
+        self.con.set_client_encoding("utf-8")
+        # カーソルを作成する
+        self.cursor = self.con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    def execute_non_query(self, sql: str, bind_var: tuple = None) -> None:
+        """CREATE/INSERT/UPDATE/DELETEのSQL実行メソッド
+        :param sql: 実行SQL
+        :param bind_var: バインド変数
+        :return: None
+        """
+        # SQLの実行
+        if bind_var is None:
+            self.cursor.execute(sql)
+        else:
+            # バインド変数がある場合は指定して実行
+            self.cursor.execute(sql, bind_var)
+    def execute_query(self, sql: str, bind_var: tuple = None, count: int = 0) -> list:
+        """SELECTのSQL実行メソッド
+        :param sql: 実行SQL
+        :param bind_var: バインド変数
+        :param count: データ取得件数
+        :return: 結果リスト
+        """
+        # SQLの実行
+        if bind_var is None:
+            self.cursor.execute(sql)
+        else:
+            # バインド変数がある場合は指定して実行
+            self.cursor.execute(sql, bind_var)
+        result = []
+        if count == 0:
+            rows = self.cursor.fetchall()
+            for row in rows:
+                result.append(dict(row))
+        else:
+            # 件数指定がある場合はその件数分を取得する
+            rows = self.cursor.fetchmany(count)
+            for row in rows:
+                result.append(dict(row))
+        return result
+    def commit(self) -> None:
+        """コミット
+        :return: None
+        """
+        self.con.commit()
+    def rollback(self) -> None:
+        """ロールバック
+        :return: None
+        """
+        self.con.rollback()
+    def __del__(self) -> None:
+        """デストラクタ
+        :return: None
+        """
+        self.cursor.close()
+        self.con.close()
